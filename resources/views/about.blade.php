236022582730ec19d0911b4d58d576ba85abdb30
andrea scacchi <andrea.scacchi@live.com>
2022-07-14 17:00:38 +0200

    added another route in standard html

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ABOUT PAGE</title>
+</head>
+<body>
+    <h1>This is the ABOUT PAGE</h1>
+
+</body>
+<style>
+    h1{
+        color: red;
+        width: 100%;
+        margin: auto;
+    }
+</style>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
